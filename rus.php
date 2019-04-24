0816a4b4bcbe4e5a7effbff9427c33eaf0d67307
Ruslan Gusseinov <r.gusseinov@mail.ru>
2019-04-24 17:24:49 +0600

    sdfsdf

--- rus.php
+++ rus.php
@@ -1,3 +1,10 @@
 <?
   echo "zdes pro sport pitanie !asd";
+
+  error_reporting(E_ALL);
+  include('custom_connectdb.php');
+  include('Vtiger/crm_data_arrange.php');  
+  date_default_timezone_set("UTC");
+  global $perDate;
+  
 ?>
