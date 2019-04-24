8097ddd1da3a83f05dbbcf4610430ee8b65310fa
Ruslan Gusseinov <r.gusseinov@mail.ru>
2019-04-24 17:27:30 +0600

    added date condition

--- rus.php
+++ rus.php
@@ -6,5 +6,17 @@
   include('Vtiger/crm_data_arrange.php');  
   date_default_timezone_set("UTC");
   global $perDate;
+
+  $repUsers = $_REQUEST['users'];
+  
+  $dateFrom = $_REQUEST['dateFrom'];
+  $dateTo = $_REQUEST['dateTo'];
+  
+  if (($dateFrom) && ($dateTo)){
+    $perDate = " vtiger_crmentity.createdtime BETWEEN '$dateFrom' AND '$dateTo' ";
+	$headr_date = 'From ' . $dateFrom . ' Till '.$dateTo;
+  } else  if (($dateFrom) && (!$dateTo)){
+    $perDate = " vtiger_crmentity.createdtime = '$dateFrom'";
+  } 
   
 ?>
